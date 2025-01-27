94815fee2afa15ce9ab8b00bbfc778b33dc94abe
Karli Acuña <karliacuna@MacBook-Air-de-Karli.local>
2025-01-27 16:38:35 -0300

    Cambios en boton de filtro SwitchVideo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+	output: 'server', // Configurar el modo como HYBRID
 	integrations: [mdx(), sitemap(), react()],
 });
+
