a48075cb714a7a80f4dbabf55e8bd7fe679c5ee1
Karli Acuña <karliacuna@MacBook-Air-de-Karli.local>
2025-01-26 23:12:45 -0300

    Se agrega página dinámica por video y se incluye MVP de tutorial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue}',
+            './node_modules/flowbite/**/*.js'
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+module.exports = {
+  darkMode: 'class',
+}
+
