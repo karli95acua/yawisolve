5285a39669ecaf2c8ca2740d9465888428a539a5
Gabriel Patricio Muñoz Orellana <gmunoz@yawi.ai>
2025-01-28 13:42:38 -0300

    Subiendo yawisolve-main como repositorio principal

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  output: 'server', // Modo Server-Side Rendering
+  adapter: node({
+    mode: 'standalone', // Configuración para Node.js (standalone es comúnmente usado)
+  }), // Adaptador para Node.js
+  integrations: [mdx(), sitemap(), react()],
+});
